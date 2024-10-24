05f3f828087082dafd463f8bab13fa3456f82696
Jona <jonatanemanuelsilva6@gmail.com>
2024-10-24 20:31:02 -0300

    add pedir datos usuario main.js

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 const args = process.argv.slice(2)
 
 if (args.length < 2) {
-    console.log("Por favor, ingrese 2 valores numericos. Ej: `2 5`"); 
+    console.log("Por favor, ingrese 2 valores numericos."); 
+    console.log("Por favor, la cuenta que quiere realizar."); 
     process.exit;
 } 
 
 const a = args[0]
 const b = args[1]
+const operador = args[2];
 
-const sumar = require('./sumar');
-const restar = require('./restar');
-const multiplicar = require('./multiplicar');
-const dividir = require('./dividir');
+// switch (operador) {
 
- console.log(sumar(2, 3)); // 5
- console.log(restar(5, 2)); // 3
- console.log(multiplicar(4, 3)); // 12
- console.log(dividir(10, 2)); // 5
+//     case "+":
+//         result = 
+// }
+
+
+
+
+// const sumar = require('./sumar');
+// const restar = require('./restar');
+// const multiplicar = require('./multiplicar');
+// const dividir = require('./dividir');
+
+//  console.log(sumar(2, 3)); // 5
+//  console.log(restar(5, 2)); // 3
+//  console.log(multiplicar(4, 3)); // 12
+//  console.log(dividir(10, 2)); // 5
+
