92cc47b2ac662688e64849550fb6e19ff3a65f84
Jona <jonatanemanuelsilva6@gmail.com>
2024-10-24 19:54:39 -0300

    add estrucutra main

---
+++ main.js
@@ -0,0 +1,9 @@
+const sumar = require('./sumar');
+const restar = require('./restar');
+const multiplicar = require('./multiplicar');
+const dividir = require('./dividir');
+
+ console.log(sumar(2, 3)); // 5
+ console.log(restar(5, 2)); // 3
+ console.log(multiplicar(4, 3)); // 12
+ console.log(dividir(10, 2)); // 5
